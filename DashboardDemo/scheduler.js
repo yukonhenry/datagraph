f06aea0a9411f05b61c40691ccc8f8a6e2047153
yukonhenry <htominaga@gmail.com>
2013-04-23 00:51:51 -0700

    implement returning scheduled matches to web app

--- DashboardDemo/scheduler.js
+++ DashboardDemo/scheduler.js
@@ -9,11 +9,23 @@ require(["dojo/dom", "dojo/on", "dojo/parser", "dijit/registry","dojo/ready",
 		var getSchedule = function(evt) {
 			var numTeams = registry.byId("numberTeams").get("value");
 			var numVenues = registry.byId("numberVenues").get("value");
-			var textarea = dom.byId("schedule_textarea");
+			var schedulerDiv = dom.byId("schedulerDiv");
 	        script.get("http://127.0.0.1:8080/getschedule", {
 	        	jsonp:"callback", query: {num_teams:numTeams, num_venues:numVenues}
 	        }).then(function(data) {
-	        	textarea.value = data.game_list;
+	        	// data returned from server is an array of tuples, with each tuple
+	        	// representing two teams in a match
+	        	d3.select(schedulerDiv).selectAll("p")
+					.data(data.game_list).enter()
+					.append("p")
+					.text(function(d,i) {
+						var matchstr = "slot "+i+": ";
+						for (var j =0; j < d.length; j++) {
+							var match = d[j];
+							matchstr += match[0]+"vs"+match[1]+" ";
+						}
+						return matchstr;
+					});
 			});
 		}
 		ready(function() {
