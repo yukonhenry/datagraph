9cb8e5497afb8aa07c8004c3fb3b1a664b07bb43
yukonehenry <htominaga@gmail.com>
2013-04-26 13:57:44 -0700

    small update

--- LeagueScheduler/scheduler.js
+++ LeagueScheduler/scheduler.js
@@ -2,11 +2,13 @@
  * @author Henry
  */
 require(["dojo/on", "dojo/parser", "dijit/registry","dojo/ready", "dgrid/Grid",
+		"dojo/store/JsonRest",
 		"dijit/form/Select", "dijit/form/NumberTextBox","dijit/form/Button",
 		"dojo/domReady!"],
-	function(on, parser, registry, ready, Grid) {
+	function(on, parser, registry, ready, Grid, JsonRest) {
 		var playdivSelectId, numberTeamsId, numberVenuesId;
 		var numTeams = 0; numVenues =0; divnum = "U5";
+		
 		function setDivTeams() {
 			console.log("dn, nt, nv="+divnum+" "+numTeams+" "+numVenues);
 			
