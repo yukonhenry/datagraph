9fe72b9f37ecefdbae4904283058b3b38da2b994
yukonehenry <htominaga@gmail.com>
2013-04-25 18:38:56 -0700

    create separate project 'League Scheduler' that re-implements from scratch using local dojo (1.9.x RC0 and Dgrid) which was problematic with original Dashboardemo project

---
+++ LeagueScheduler/scheduler.js
@@ -0,0 +1,48 @@
+/**
+ * @author Henry
+ */
+require(["dojo/on", "dojo/parser", "dijit/registry","dojo/ready", "dgrid/Grid",
+		"dijit/form/Select", "dijit/form/NumberTextBox","dijit/form/Button",
+		"dojo/domReady!"],
+	function(on, parser, registry, ready, Grid) {
+		var playdivSelectId, numberTeamsId, numberVenuesId;
+		var numTeams = 0; numVenues =0; divnum = "U5";
+		function setDivTeams() {
+			console.log("dn, nt, nv="+divnum+" "+numTeams+" "+numVenues);
+			
+			var divdata = [
+				{dnum:divnum, nteams:numTeams, nvenues:numVenues}
+			];
+			var grid = new Grid({
+				columns: {
+					dnum: "Division", nteams: "Number Teams", nvenues: "Number Venues"
+				}
+			}, "divisionInfoGrid");
+			grid.renderArray(divdata);
+
+		}
+
+		var setPlayDivSelect = function(evt) {
+			divnum = playdivSelectId.get("value");
+			setDivTeams();
+		};
+
+		var setNumberTeams = function(evt) {
+			numTeams = numberTeamsId.get("value");
+			setDivTeams();
+		}
+		var setNumberVenues = function(evt) {
+			numVenues = numberVenuesId.get("value");
+			setDivTeams();
+		}
+		ready(function() {
+ 			parser.parse();	
+			playdivSelectId = registry.byId("playdivisionSelect");
+ 			on(playdivSelectId, "change", setPlayDivSelect);
+ 			numberTeamsId = registry.byId("numberTeams");
+ 			on(numberTeamsId, "change", setNumberTeams);
+ 			numberVenuesId = registry.byId("numberVenues");
+ 			on(numberVenuesId, "change", setNumberVenues);
+ 		}); 
+	}
+);
