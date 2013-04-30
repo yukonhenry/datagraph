474d6916e27e5770cd6550525df55d0eff66f838
yukonehenry <htominaga@gmail.com>
2013-04-30 10:45:59 -0700

    further progress on dgrid implementation

--- LeagueScheduler/scheduler.js
+++ LeagueScheduler/scheduler.js
@@ -1,55 +1,76 @@
 /**
  * @author Henry
  */
-require(["dojo/on", "dojo/parser", "dijit/registry","dojo/ready", "dgrid/Grid",
-		"dojo/store/JsonRest", "dojo/request", "dojo/request/script",
+// small note, if there are issues with garbage characters in the jsonp script get request,
+// ensure http://bugs.dojotoolkit.org/ticket/16408 has been resolved in the branch/release
+// that is being used.
+require(["dojo/on", "dojo/parser", "dijit/registry","dojo/ready", 
+		"dojo/_base/declare", "dgrid/Grid", "dgrid/Selection",
+		"dojo/request/script", 
 		"dijit/form/Select", "dijit/form/NumberTextBox","dijit/form/Button",
 		"dojo/domReady!"],
-	function(on, parser, registry, ready, Grid, JsonRest, request, script) {
+	function(on, parser, registry, ready, declare, Grid, Selection, script) {
 		var playdivSelectId, numberTeamsId, numberVenuesId;
 		var numTeams = 0; numVenues =0; divnum = "U5";
+		
+		var CustomGrid = declare([ Grid, Selection ]);
+		var grid = new CustomGrid({
+			columns: {
+				agediv:"Age Group",
+				gender:"Boy/Girl",
+				totalteams:"Total#",
+				totalfields:"Field#",
+				gamedaysperweek:"Weekly Games#"
+			},
+			selectionMode: "single"		
+		}, "divisionInfoGrid");
 		
 		script.get("http://127.0.0.1:8080/leaguedivinfo", {
 			jsonp:"callback"
 		}).then(function(ldata){
-			console.log("ldata="+ldata.leaguedivinfo);
+			ldata_array = ldata.leaguedivinfo;
+			grid.renderArray(ldata_array);
+			grid.on("dgrid-select", function(event){
+    // Report the item from the selected row to the console.
+    console.log("Row selected: ", event.rows[0].data);
+});
+grid.on("dgrid-deselect", function(event){
+    console.log("Row de-selected: ", event.rows[0].data);
+});
 		});
-		function setDivTeams() {
-			console.log("dn, nt, nv="+divnum+" "+numTeams+" "+numVenues);
-			
-			var divdata = [
-				{dnum:divnum, nteams:numTeams, nvenues:numVenues}
-			];
-			var grid = new Grid({
-				columns: {
-					dnum: "Division", nteams: "Number Teams", nvenues: "Number Venues"
-				}
-			}, "divisionInfoGrid");
-			grid.renderArray(divdata);
 
-		}
+/*		
+		var getSchedule = function(evt) {
 
-		var setPlayDivSelect = function(evt) {
-			divnum = playdivSelectId.get("value");
-			setDivTeams();
-		};
-
-		var setNumberTeams = function(evt) {
-			numTeams = numberTeamsId.get("value");
-			setDivTeams();
+	        script.get("http://127.0.0.1:8080/getschedule", {
+	        	jsonp:"callback", query: {num_teams:numTeams, num_venues:numVenues}
+	        }).then(function(data) {
+	        	if (game_listP) {
+	        		d3.select(schedulerDiv).selectAll("p").remove();
+	        	}
+	        	// data returned from server is an array of tuples, with each tuple
+	        	// representing two teams in a match
+	        	// ref https://github.com/mbostock/d3/wiki/Selections#wiki-exit
+	        	// on how to assign variable before enter() so that a check
+	        	// can be made later (above) to see if added paragraphs need to be deleted.
+	        	game_listP = d3.select(schedulerDiv).selectAll("p")
+	        						.data(data.game_list);
+	        	game_listP.enter()
+					.append("p")
+					.text(function(d,i) {
+						var matchstr = "slot "+i+": ";
+						for (var j =0; j < d.length; j++) {
+							var match = d[j];
+							matchstr += match[0]+"vs"+match[1]+" ";
+						}
+						return matchstr;
+					});
+			});
 		}
-		var setNumberVenues = function(evt) {
-			numVenues = numberVenuesId.get("value");
-			setDivTeams();
-		}
+*/
 		ready(function() {
  			parser.parse();	
-			playdivSelectId = registry.byId("playdivisionSelect");
- 			on(playdivSelectId, "change", setPlayDivSelect);
- 			numberTeamsId = registry.byId("numberTeams");
- 			on(numberTeamsId, "change", setNumberTeams);
- 			numberVenuesId = registry.byId("numberVenues");
- 			on(numberVenuesId, "change", setNumberVenues);
+			//on(registry.byId("schedule_btn"), "click", getSchedule);
  		}); 
 	}
 );
