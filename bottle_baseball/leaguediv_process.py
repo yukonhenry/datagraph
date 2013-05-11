1d87ce003a9b018e3594e7e2c8398f7bd635dcd8
yukonehenry <htominaga@gmail.com>
2013-05-10 17:29:59 -0700

    implement pymongodb and basic interface; button schedules all divisions; row selection retrieves document from mongodb

--- bottle_baseball/leaguediv_process.py
+++ bottle_baseball/leaguediv_process.py
@@ -7,6 +7,11 @@ from scheduler import ScheduleGenerator
 # http://api.mongodb.org/python/current/tutorial.html
 from pymongo import  *
 
+# prep for connecting to db
+client = MongoClient()
+testschedule_db = client.testschedule_db
+div_schedule_collect = testschedule_db.div_schedule
+
 def get_leaguedata():
     fname = 'leaguediv_json.txt'
     json_file = open(fname)
@@ -39,8 +44,12 @@ def leaguedivinfo(tid):
             nt = div['totalteams']
             nv = len(div['fields'])
             interval = div['gameinterval']
+            age = div['agediv']
+            gender = div['gender']
+            division_data = div_schedule_collect.find_one({'age':age, 'gender':gender})
+            game_list = division_data['game_list']
             scheduler = ScheduleGenerator(nt, nv, interval)
-            game_list = scheduler.generateRRSchedule()
+            #game_list = scheduler.generateRRSchedule()
             ha_counter = getattr(scheduler, 'metrics_list')
             print ha_counter
             a = json.dumps({"game_list":game_list, "numFields":nv})
@@ -55,14 +64,11 @@ def get_alldivSchedule():
     ldata = get_leaguedata()
     ldata_divinfo = ldata['leaguedivinfo']
 
-    # prep for connecting to db
-    client = MongoClient()
-    testschedule_db = client.testschedule_database
-    div_schedule_collect = testschedule_db.div_schedule
     # http://docs.mongodb.org/manual/tutorial/create-a-unique-index/
     # and pymango doc http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.ensure_index
     # http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.create_index
-    div_schedule_collect.create_index([('age', ASCENDING),('gender',ASCENDING)])
+    # apparently the need to create a unique index is not needed if an upsert (see below) call is made.
+    #div_schedule_collect.create_index([('age', ASCENDING),('gender',ASCENDING)], unique=True, dropDups=True)
     for div in ldata_divinfo:
         nt = div['totalteams']
         nv = len(div['fields'])
@@ -72,7 +78,9 @@ def get_alldivSchedule():
 
         age = div['agediv']
         gender = div['gender']
-        db_id = div_schedule_collect.insert({'age':age, 'gender':gender, 'game_list':game_list}, safe=True)
+        # use upsert with upsert flag enabled so that first call will create insert, but subsequent calls will over-write
+        # ref http://docs.mongodb.org/manual/core/create/
+        db_id = div_schedule_collect.update({'age':age, 'gender':gender}, {'age':age, 'gender':gender, 'game_list':game_list}, safe=True, upsert=True)
         print 'db_id=', db_id
         #ha_counter = getattr(scheduler, 'metrics_list')
         #print ha_counter
