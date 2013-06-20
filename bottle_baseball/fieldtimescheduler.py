46adc42f952b8711ec15dd3d8e0048f908b85a80
yukonehenry <htominaga@gmail.com>
2013-06-19 18:42:49 -0700

    field time scheduler implementation

--- bottle_baseball/fieldtimescheduler.py
+++ bottle_baseball/fieldtimescheduler.py
@@ -1,8 +1,10 @@
 from datetime import  datetime, timedelta
+from itertools import cycle
+from schedule_util import roundrobin, all_same
+#ref Python Nutshell p.314 parsing strings to return datetime obj
+from dateutil import parser
 firstgame_starttime_CONST = datetime(2013,9,1,8,0,0)   # 8am on a dummy date
 start_time_CONST = 'START_TIME'
-from itertools import cycle
-from schedule_util import roundrobin
 venue_game_list_CONST = 'VENUE_GAME_LIST'
 gameday_id_CONST = 'GAMEDAY_ID'
 gameday_data_CONST = 'GAMEDAY_DATA'
@@ -24,6 +26,7 @@ class FieldTimeScheduleGenerator:
         self.leaguedivinfo = leaguedivinfo
         self.connected_div_components = connected_comp
         self.scheduleMatrix = []
+        self.fieldinfo = fieldinfo
         # list hold per-field data structure of available time slots for scheduling
         # assume greeday algorithm for now
         for field in fieldinfo:
@@ -35,44 +38,73 @@ class FieldTimeScheduleGenerator:
         # for finding index of dictionary key in array of dictionaries
         # use indexer so that we don't depend on order of divisions in total_match_list
         # alternate method http://stackoverflow.com/questions/3179106/python-select-subset-from-list-based-on-index-set
+        # indexer below is used to protect against list of dictionaries that are not ordered according to id,
+        # though it is a protective measure, as the list should be ordered with the id.
         match_list_indexer = dict((p['div_id'],i) for i,p in enumerate(total_match_list))
         leaguediv_indexer = dict((p['div_id'],i) for i,p in enumerate(self.leaguedivinfo))
+        fieldinfo_indexer = dict((p['field_id'],i) for i,p in enumerate(self.fieldinfo))
         for connected_div_list in self.connected_div_components:
             fset = set() # set of shared fields
             submatch_list = []
             submatch_len_list = []
+            gameinterval_dict = {}
             for division in connected_div_list:
                 divindex = leaguediv_indexer.get(division)
                 fset.update(self.leaguedivinfo[divindex]['fields'])  #incremental union
+                # http://docs.python.org/2/library/datetime.html#timedelta-objects
+                # see also python-in-nutshell
+                # convert gameinterval into datetime.timedelta object
+                ginterval = self.leaguedivinfo[divindex]['gameinterval']
+                gameinterval_dict[division] = timedelta(0,0,0,0,ginterval)
                 index = match_list_indexer.get(division)
                 submatch_list.append(total_match_list[index])
                 submatch_len_list.append(len(total_match_list[index]['match_list']))
-            print 'submatch', submatch_len_list
+            if not all_same(submatch_len_list):
+                print 'different number of games per season amongst shared field NOT SUPPORTED'
+                return None
+
             flist = list(fset)
             flist.sort()  # default ordering, use it for now
-            fieldlist_iter = cycle(flist)
-            # max below is not sufficient
+            field_list = []
+            initialstarttime_dict = {}
+            for f_id in flist:
+                # instead of creating indexer, be lazy and assume fields are in order of field_id
+                # field_id is 0-index based
+                findex = fieldinfo_indexer.get(f_id)
+                # note convert start time string to datetime obj
+                nexttime_dtime = parser.parse(self.fieldinfo[findex]['start_time'])
+                field_list.append({'field_id':f_id,
+                                   'next_time':nexttime_dtime})
+                initialstarttime_dict[f_id] = nexttime_dtime  #use for resetting time to beginning of day
+            print 'fieldlist', field_list
+            # max below is not sufficient if there are differences in season per games
+            # within the divisions that share fields
             for round_index in xrange(max(submatch_len_list)):
                 combined_match_list = []
                 for division_dict in submatch_list:
                     div_id = division_dict['div_id']
                     match_list = division_dict['match_list'][round_index]
                     round_id = match_list[round_id_CONST]
+                    print 'round id', round_id
                     game_list = match_list[game_team_CONST]
                     round_match_list = []
                     for game in game_list:
-                        round_match_list.append({'div_id':div_id, 'game':game})
+                        round_match_list.append({'div_id':div_id, 'game':game,
+                                                 'gameinterval':gameinterval_dict[div_id]})
                     combined_match_list.append(round_match_list)
+                # mutliplex the divisions that are sharing fields; user itertools round robin utility
                 #http://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion
                 # http://stackoverflow.com/questions/7529376/pythonic-way-to-mix-two-lists
-                print 'combined match list',combined_match_list
-                testobj = roundrobin(mlist for mlist in combined_match_list)
-                for i in testobj:
-                    print 'rr',i
-                #matchdiv = matchdiv_list_iter.next()
-                field = fieldlist_iter.next()
-                #print 'match', matchdiv
-                #print 'field', field
+                rrgenobj = roundrobin(combined_match_list)
+                field_list_iter = cycle(field_list)
+                field = field_list_iter.next()
+                for rrgame in rrgenobj:
+                    print 'rr',rrgame, field, field['next_time'].strftime(time_format_CONST)
+                    field['next_time'] += rrgame['gameinterval']
+                    field = field_list_iter.next()
+                # reset start time for next round of games
+                for f in field_list:
+                    f['next_time'] = initialstarttime_dict[field['field_id']]
 
     def generateRRSchedule(self, conflict_ind=0):
         self.generateRoundMatchList()
--- bottle_baseball/schedule_util.py
+++ bottle_baseball/schedule_util.py
@@ -1,11 +1,12 @@
 from itertools import cycle, islice
 # ref http://docs.python.org/2/library/itertools.html
 # ref http://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion
-def roundrobin(*iterables):
+# modified to assume argument is list of lists (instead of comma separated lists)
+def roundrobin(iterable_list):
     "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
     # Recipe credited to George Sakkis
-    pending = len(iterables)
-    nexts = cycle(iter(it).next for it in iterables)
+    pending = len(iterable_list)
+    nexts = cycle(iter(it).next for it in iterable_list)
     while pending:
         try:
             for next in nexts:
@@ -13,3 +14,8 @@ def roundrobin(*iterables):
         except StopIteration:
             pending -= 1
             nexts = cycle(islice(nexts, pending))
+
+
+# ref http://stackoverflow.com/questions/3787908/python-determine-if-all-items-of-a-list-are-the-same-item
+def all_same(items):
+    return all(x == items[0] for x in items)
