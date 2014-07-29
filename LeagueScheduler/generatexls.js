6f74c5928092198705d7e51adbf5296017bc8a4b
Henry Tominaga <htominaga@gmail.com>
2014-07-29 07:51:13 -0700

    update adding xlspane widgets

--- LeagueScheduler/generatexls.js
+++ LeagueScheduler/generatexls.js
@@ -5,17 +5,17 @@
 // define class for creating pane(s) to host links to xls hardcopy files for the
 // generated schedule
 define(["dbootstrap", "dojo/dom", "dojo/_base/declare", "dojo/_base/lang",
-	"dojo/_base/array", "dijit/registry", "dijit/DropDownMenu",
+	"dojo/_base/array", "dijit/registry", "dijit/DropDownMenu", "dijit/MenuItem",
 	"dijit/form/DropDownButton", "dijit/layout/ContentPane",
 	"LeagueScheduler/idmgrSingleton",
 	"put-selector/put", "dojo/domReady!"],
 	function(dbootstrap, dom, declare, lang, arrayUtil, registry,
-		DropDownMenu, DropDownButton, ContentPane, idmgrSingleton, put) {
+		DropDownMenu, MenuItem, DropDownButton, ContentPane, idmgrSingleton, put) {
 		var constant = {
 			idproperty_str:'xls_id',
 		};
 		var idconstant = {
-
+			ddown_btn_id:"xlscpane_ddown_btn_id"
 		}
 		return declare(null, {
 			idproperty:constant.idproperty_str, idmgr_obj:null, op_type:"",
@@ -31,14 +31,29 @@ define(["dbootstrap", "dojo/dom", "dojo/_base/declare", "dojo/_base/lang",
 				}
 			},
 			generate_xlscpane_widgets: function(xls_cpane) {
-				var ddown_menu = new DropDownMenu({
-				})
-				var ddown_btn = new DropDownButton({
-					class:"info editsched",
-					label:"Select XLS Output",
-					dropDown:ddown_menu
-				})
-				xls_cpane.addChild(ddown_btn);
+				var ddown_btn = registry.byId(this.opconstant_obj.ddown_btn_id);
+				if (!ddown_btn) {
+					// create widgets only if they don't already exist
+					var ddown_menu = new DropDownMenu({
+					})
+					var ddown_btn = new DropDownButton({
+						class:"info editsched",
+						label:"Select XLS Output",
+						dropDown:ddown_menu,
+						id:this.opconstant_obj.ddown_btn_id
+					})
+					xls_cpane.addChild(ddown_btn);
+					var label_id_list = [
+						{label:"by Division", genxls_id:'div_id'},
+						{label:"by Field", genxls_id:'field_id'},
+						{label:"by Team", genxls_id:'team_id'}];
+					arrayUtil.forEach(label_id_list, function(item) {
+						var menuitem_widget = new MenuItem({
+							label:item.label,
+						})
+					})
+				}
+
 			}
 		})
 	}
