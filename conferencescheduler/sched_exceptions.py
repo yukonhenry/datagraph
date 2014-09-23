f14fed0b69c0788b91591d604ec89917035e6b6d
yukonehenry <htominaga@gmail.com>
2014-09-22 20:08:40 -0700

    add conf scheduling prototype

---
+++ conferencescheduler/sched_exceptions.py
@@ -0,0 +1,59 @@
+''' Copyright YukonTR 2013 '''
+# ref http://docs.python.org/2/tutorial/errors.html
+# http://www.ibiblio.org/g2swap/byteofpython/read/raising-exceptions.html
+class FieldAvailabilityError(Exception):
+    '''A user-defined exception class.'''
+    def __init__(self, div_id):
+        Exception.__init__(self)
+        self.div_id = div_id
+    def __str__(self):
+        return repr(self.div_id)
+
+class TimeSlotAvailabilityError(Exception):
+    '''A user-defined exception class.'''
+    def __init__(self, field_id, round_id):
+        Exception.__init__(self)
+        self.field_id = field_id
+        self.round_id = round_id
+    def __str__(self):
+        return "field "+str(self.field_id)+" gameday "+str(self.round_id)+" has no more slots"
+
+class TimeCompactionError(Exception):
+    '''Raised if there is some problem compacting the time schedule
+    (eliminating time gamps)'''
+    def __init__(self, field_id, gameday_id):
+        Exception.__init__(self)
+        self.field_id = field_id
+        self.gameday_id = gameday_id
+
+class FieldConsistencyError(Exception):
+    '''Raised if there is some unexpected inconsistency for allocated fields for a division'''
+    def __init__(self, field_list1, field_list2):
+        Exception.__init__(self)
+        self.field_list1 = field_list1
+        self.field_list2 = field_list2
+
+class FieldTimeAvailabilityError(Exception):
+    ''' Raised if there is a general lack of fields or time to service required number of games'''
+    def __init__(self, msg, div_list):
+        Exception.__init__(self)
+        self.div_list = div_list
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg) + "div_list="+repr(self.div_list)
+
+class CodeLogicError(Exception):
+    ''' Generic Exception if there is a logic error in code '''
+    def __init__(self, msg):
+        Exception.__init__(self)
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+
+class SchedulerConfigurationError(Exception):
+    '''A user-defined exception class.'''
+    def __init__(self, msg):
+        Exception.__init__(self)
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
--- conferencescheduler/scheduler.py
+++ conferencescheduler/scheduler.py
@@ -3,19 +3,29 @@
 import gspread
 from operator import itemgetter
 import re
+from pprint import pprint
+from dateutil import parser
+from sched_exceptions import CodeLogicError
 STUDENT_NAME = 'SBA Student Name'
 LAST_NAME = 'Last Name'
 FIRST_NAME = 'First Name'
+TIMESTAMP = 'Timestamp'
 TEACHER_HEADER = 'Teacher/Coach Office Hours'
 TEACHERNAME_PATTERN = "([\w.]+ [\w.]+) -"
+COACH_NAME_LIST = ['Martin Benes', 'Jeff Schloss', 'Seth McCadam', 'Jeff Kai', 'Devin Gill', 'Karen Lundgren', 'Katharina Golik', 'Trevor Tanhoff', 'Caitlin Curran']
+MAX_SLOTS = 12
 def confsched():
     gc = gspread.login("htominaga@gmail.com", "bxoausumpwtuaqid")
     entrysheet = gc.open("Sunday11_21_2014SBA").sheet1
+    # get the entire sheet
     signup_list = entrysheet.get_all_records()
+    # sort by student name column
     signup_list.sort(key=itemgetter(STUDENT_NAME))
     student_name_list = list()
-    student_teacher_map_list = list()
-    stindexerMatch = lambda x: [i for i,p in enumerate(student_teacher_map_list) if p['student_name']==x]
+    raw_student_teacher_map_list = list()
+    stindexerMatch = lambda x: [i for i,p in enumerate(raw_student_teacher_map_list) if p['student_name']==x]
+    # sanitize student name list
+    signup_list_len = len(signup_list)
     for signup_dict in signup_list:
         student_name = signup_dict[STUDENT_NAME]
         if type(student_name) is str:
@@ -23,25 +33,141 @@ def confsched():
             if name_len == 1:
                 student_name += " " + signup_dict[LAST_NAME]
         else:
-            name_len = 0
+            # if student name entry is not valid, default name to last name of
+            # person that entered info
             student_name = signup_dict[LAST_NAME]
         student_name_list.append(student_name)
         teacher_descrip_list_string = signup_dict[TEACHER_HEADER]
         if teacher_descrip_list_string:
             teacher_name_list = re.findall(TEACHERNAME_PATTERN, teacher_descrip_list_string)
-        student_teacher_map_list.append({'student_name':student_name,
-            'teacher_name_list':teacher_name_list})
+        else:
+            teacher_name_list = None
+        raw_student_teacher_map_list.append({'student_name':student_name,
+            'teacher_name_list':teacher_name_list,
+            'timestamp':parser.parse(signup_dict[TIMESTAMP])})
         print student_name, teacher_name_list
         print "--------------------------"
-    print student_teacher_map_list
+    print raw_student_teacher_map_list
+    # manage duplicate entries of student name
     dup_student_name_set = set([x for x in student_name_list if student_name_list.count(x) > 1])
+    dup_student_name_set_len = len(dup_student_name_set)
     print dup_student_name_set
+
+    dup_student_teacher_map_dict = dict()
     for student_name in dup_student_name_set:
         index_list = stindexerMatch(student_name)
         teacher_name_set = set()
         for index in index_list:
-            teacher_name_set.update(student_teacher_map_list[index]['teacher_name_list'])
-        teacher_name_list = list(teacher_name_set)
-        print student_name, teacher_name_list
+            tn_list = raw_student_teacher_map_list[index]['teacher_name_list']
+            if tn_list is not None:
+                teacher_name_set.update(tn_list)
+        teacher_name_list = list(teacher_name_set) if teacher_name_set else None
+        dup_student_teacher_map_dict[student_name] = teacher_name_list
+    print dup_student_teacher_map_dict
+
+    norm_student_teacher_map_list = list()
+    dupseen_set = set()
+    noteacher_count = 0
+    for entry in raw_student_teacher_map_list:
+        student_name = entry['student_name']
+        if student_name not in dup_student_name_set:
+            if entry['teacher_name_list']:
+                norm_student_teacher_map_list.append(entry)
+            else:
+                noteacher_count += 1
+        elif student_name not in dupseen_set:
+            teacher_name_list = dup_student_teacher_map_dict[student_name]
+            if teacher_name_list:
+                norm_student_teacher_map_list.append({'student_name':student_name,
+                    'teacher_name_list':teacher_name_list,
+                    'timestamp':entry['timestamp']})
+            else:
+                noteacher_count += 1
+            dupseen_set.add(student_name)
+    norm_student_teacher_map_list.sort(key=itemgetter('timestamp'))
+    pprint(norm_student_teacher_map_list)
+    print 'original len', signup_list_len, 'dup entries', dup_student_name_set_len, 'normlen', len(norm_student_teacher_map_list), 'noconf', noteacher_count
+
+    #----------------------------------------------
+    #-Make Assignments
+    student_schedule_list = list()
+    teacher_schedule_list = list()
+    teacher_seen_set = set()
+    for st_map in norm_student_teacher_map_list:
+        student_name = st_map['student_name']
+        ssched_list = [{'sched_flag':False, 'teacher_name':""} for x in range(MAX_SLOTS)]
+        teacher_name_list = st_map['teacher_name_list']
+        for teacher_name in teacher_name_list:
+            if teacher_name not in teacher_seen_set:
+                default_teachersched_list = [{'sched_flag':False, 'student_name':""} for x in range(MAX_SLOTS)]
+                teacher_seen_set.add(teacher_name)
+                teacher_schedule_list.append({'teacher_name':teacher_name, 'schedule_list':default_teachersched_list, 'earliest_slot':0})
+        student_teacher_schedule_list = [x for x in teacher_schedule_list if x['teacher_name'] in teacher_name_list]
+        student_teacher_schedule_list.sort(key=itemgetter('earliest_slot'))
+        student_slot_index = 0
+        studentteacher_seen_set = set()
+        for teacher_schedule in student_teacher_schedule_list:
+            teacher_name = teacher_schedule['teacher_name']
+            teacher_earliest_slot_index = teacher_schedule['earliest_slot']
+            if teacher_earliest_slot_index == student_slot_index:
+                # Match between teacher's earliest slot and the current open slot
+                # for student;
+                # first fill in the student schedule list
+                ssched_slot = ssched_list[student_slot_index]
+                if ssched_slot['sched_flag']:
+                    raise CodeLogicError("Student schedule already scheduled, should be open student %s teacher %s slot_index %d" % (student_name, teacher_name, student_slot_index))
+                ssched_slot['teacher_name'] = teacher_name
+                ssched_slot['sched_flag'] = True
+                student_slot_index += 1
+                # fill in the teacher schedule list
+                tsched_list = teacher_schedule['schedule_list']
+                tsched_slot = tsched_list[teacher_earliest_slot_index]
+                if not tsched_slot['sched_flag']:
+                    tsched_slot['student_name'] = student_name
+                    tsched_slot['sched_flag'] = True
+                    teacher_schedule['earliest_slot'] += 1
+                    studentteacher_seen_set.add(teacher_name)
+                else:
+                    raise CodeLogicError("teacher and student slots match, but teacher slot already scheduled")
+            elif teacher_earliest_slot_index < student_slot_index:
+                # note teacher schedule list is sorted in ascending order according
+                # to earliest_slot
+                tsched_list = teacher_schedule['schedule_list']
+                tsched_slot = tsched_list[student_slot_index]
+                if not tsched_slot['sched_flag']:
+                    # unscheduled slot, use it
+                    ssched_slot = ssched_list[student_slot_index]
+                    if ssched_slot['sched_flag']:
+                        raise CodeLogicError("Student schedule already scheduled, should be open student %s teacher %s teacher earliest slot index %d student_slot_index %d" % (student_name, teacher_name, teacher_earliest_slot_index, student_slot_index))
+                    else:
+                        ssched_slot['teacher_name'] = teacher_name
+                        ssched_slot['sched_flag'] = True
+                        student_slot_index += 1
+                        tsched_slot['student_name'] = student_name
+                        tsched_slot['sched_flag'] = True
+                        studentteacher_seen_set.add(teacher_name)
+                else:
+                    # go to the next teacher
+                    continue
+            else:
+                # teacher earliest slot is later than student slot
+                tsched_list = teacher_schedule['schedule_list']
+                tsched_slot = tsched_list[teacher_earliest_slot_index]
+                if not tsched_slot['sched_flag']:
+                    # unscheduled slot, use it
+                    ssched_slot = ssched_list[teacher_earliest_slot_index]
+                    if ssched_slot['sched_flag']:
+                        raise CodeLogicError("Student schedule already scheduled, should be open")
+                    else:
+                        ssched_slot['teacher_name'] = teacher_name
+                        ssched_slot['sched_flag'] = True
+                        #student_slot_index += 1
+                        tsched_slot['student_name'] = student_name
+                        tsched_slot['sched_flag'] = True
+                        teacher_schedule['earliest_slot'] += 1
+                        studentteacher_seen_set.add(teacher_name)
+            #print 'teacher', teacher_name, 'scheduled for student', student_name
+        student_schedule_list.append({'student_name':student_name, 'schedule_list':ssched_list})
+    #pprint(student_schedule_list)
 
 
