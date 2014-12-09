00918a36feb83c550bdc16026e2c8e097b95a130
yukonhenry <htominaga@gmail.com>
2014-12-09 13:27:09 -0800

    Revert "add dojo files" Revert adding dojo framework files This reverts commit a78c37f54050d6637f5fe97922b63ce051acb503.

--- assets/dojox/store/LocalDB.js.uncompressed.js
+++
@@ -1,8 +0,0 @@
-define("dojox/store/LocalDB", ["./db/has!indexeddb?./db/IndexedDB:./db/SQL"],
-	function(LocalDB){
-	//	module:
-	//		./store/LocalDB
-	//	summary:
-	//		The module defines an object store based on local database access
-	return LocalDB;
-});
